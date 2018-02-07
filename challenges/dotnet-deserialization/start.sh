f8eeb6c5fd17f5e42eed5f201c9b609ef7c9868a
ad <ad@vagrant>
2018-02-07 05:00:20 +0000

    Updated dotnet dockerfile

--- challenges/dotnet-deserialization/start.sh
+++ challenges/dotnet-deserialization/start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [ -d "/shared" ] && [ -v CHALLENGE_NAME ]; then
-   cp /app/restapp.dll /shared/$CHALLENGE_NAME.bin
-   md5sum --tag /app/restapp.dll > /shared/$CHALLENGE_NAME.bin.md5
+   cp /app/bin/restapp.dll /shared/$CHALLENGE_NAME.bin
+   md5sum --tag /app/bin/restapp.dll > /shared/$CHALLENGE_NAME.bin.md5
 fi;
 
 cd /app
 redir --laddr=0.0.0.0 --lport=9000 --caddr=127.0.0.1 --cport=8000 > /dev/null 2>&1 &
 
 while [ true ]; do
-   su bob -c "dotnet /app/restapp.dll"
+   su bob -c "dotnet /app/bin/restapp.dll"
    sleep 1
 done;
 
